b9290052dfd94d6c46d8bf522fb51826d2d97acd
GyanXspy <gyanaranjanpatra780@gmail.com>
2025-11-03 19:39:16 +0530

    eureka server setup

---
+++ eureka/src/test/java/com/innovation/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovation.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
